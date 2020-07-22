032da9406a11e51afbb77362515a86d45a696885
echo tom <devbytom@Desc-mb-1337.local>
2020-07-22 16:42:17 -0300

    custom response struct and json middleware

--- challenge/app/router/main.go
+++ challenge/app/router/main.go
@@ -5,18 +5,29 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+type customResponse struct {
+	Msg string `json:"msg"`
+}
+
+func fmtResponse(Msg string) customResponse {
+	return customResponse{
+		Msg,
+	}
+}
+
 //Handlers is for bundling every API endpoint
 func Handlers() *mux.Router {
 	r := mux.NewRouter()
+	r.Use(jsonMiddleware)
 	r.Use(prometheusMiddleware)
 	r.Path("/metrics").Handler(promhttp.Handler())
 
 	r.HandleFunc("/configs", getAllDataCentersHandler).Methods("GET")
 	r.HandleFunc("/configs", addDataCenterHandler).Methods("POST")
-	// r.HandleFunc("/configs/{name}", getMealByNameHandler).Methods("GET")
-	// r.HandleFunc("/configs/{name}", createMealHandler).Methods("PUT")
-	// r.HandleFunc("/configs/{name}", updateMealHandler).Methods("PATCH")
-	// r.HandleFunc("/configs/{name}", deleteMealHandler).Methods("DELETE")
+	r.HandleFunc("/configs/{name}", getMealByNameHandler).Methods("GET")
+	r.HandleFunc("/configs/{name}", createMealHandler).Methods("PUT")
+	r.HandleFunc("/configs/{name}", updateMealHandler).Methods("PATCH")
+	r.HandleFunc("/configs/{name}", deleteMealHandler).Methods("DELETE")
 
 	return r
 }
--- challenge/app/router/middlewares.go
+++ challenge/app/router/middlewares.go
@@ -24,3 +24,10 @@ func prometheusMiddleware(next http.Handler) http.Handler {
 		timer.ObserveDuration()
 	})
 }
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
